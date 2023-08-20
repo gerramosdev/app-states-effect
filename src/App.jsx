4c62f711dc1b1c8e72d92dabc70fe8d355aa0708
gerramosdev <gerramos.93@outlook.com>
2023-08-20 15:56:44 -0500

    assign winner without reset game

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,74 @@ const TURNS = {
 }
 
 
-const Square = ({children, isSelected, updateBoard, index}) => {
-  const className=`square ${isSelected} ? 'is-selected' : ''`
-  return(
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
     <div onClick={handleClick} className={className}>
-        {children}
+      {children}
     </div>
   )
 }
-const handleClick = () => {
-  updateBoard()
-}
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X);
 
-  const updateBoard = () => {
+  //identificador si hay  un ganador
+ const [winner, setWinner] = useState(null);
+
+
+ const checkWinner = (boardToCheck) => {
+   //recorremos array con las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a]&& 
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+        )
+        {
+          return(boardToCheck[a])
+        }
+      }
+    //por si no hay ganador
+    return null
+ }
+
+  const updateBoard = (index) => {
+    //corroborar si ya se hizo una jugada X o O
+    if(board[index] || winner) return
+
+    //actualizar tablero
+    const newBoard =[...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    //cambiar turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //revisar si ya alguien ganó
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      console.log(newWinner)
+      setWinner(() => {
+        return newWinner
+      })
+    }
   }
   
 
@@ -34,24 +84,47 @@ function App() {
       <h1>Triqui - Game</h1>
       <section className='game'>
         {
-          board.map((cell,index)=>{
+          board.map((square,index)=>{
             return(
               <Square
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
               >
-                
+               {square} 
               </Square>
             )
           })
         }
       </section>
       <section className='turn'>
-        <Square isSSelected={turn === TURNS.X}>
+        <Square isSelected={turn === TURNS.X}>
+        {TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>
         {TURNS.O}</Square>
 
       </section>
+      {
+        winner  !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false ? 'Empate':'Ganó:'
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+              
+              <footer>
+                <button>Empezar de nuevo</button>
+              </footer>
+            </div>
+
+          </section>
+        )
+      }
    </main>
   )
 }
